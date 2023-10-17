c34f4528e4bd9a886ba96705f05fe3a895f41b2d
shotastage <shota.shimazu@magicalsoft.app>
2023-10-17 23:51:52 +0900

    add similar accessor to Foundation UUID struct

--- Sources/UUIDv7/UUID7.swift
+++ Sources/UUIDv7/UUID7.swift
@@ -16,20 +16,21 @@ import OpenSSL
 
 public struct UUID7 {
 
-    private let timestamp48bit: Int64
+    private var timestamp48bit: Int64
+    
+    //- public var uuid: (UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8)
+    
+    public var uuidString: String
 
-    public let str: String
-
-    public init?() {
-        timestamp48bit = Int64(Date().timeIntervalSince1970 * 1000) & 0x0000_FFFF_FFFF
-
-        guard let randomData = UUID7.generateRandomBits(bitCount: 60) else { return nil }
+    public init() {
+        self.timestamp48bit = Int64(Date().timeIntervalSince1970 * 1000) & 0x0000_FFFF_FFFF
+        guard let randomData = UUID7.generateRandomBits(bitCount: 60) else { fatalError() }
 
         // Convert randomData to UInt64
         let randomBits: UInt64 = randomData.prefix(8).reversed().enumerated().reduce(0) {
             $0 + (UInt64($1.element) << (UInt64($1.offset) * 8))
         }
-
+        
         let versionAndVariantSetBits = UUID7.setVersionAndVariantBits(randomBits: randomBits)
 
         let timestampHex = String(format: "%012llX", timestamp48bit)
@@ -38,7 +39,13 @@ public struct UUID7 {
         // Split the timestamp and random bits into the UUID 8-4-4-4-12 format
         let uuidString = "\(timestampHex.prefix(8))-\(timestampHex.dropFirst(8).prefix(4))-\(randomBitsHex.prefix(4))-\(randomBitsHex.dropFirst(4).prefix(4))-\(randomBitsHex.dropFirst(8))"
 
-        str = uuidString
+        self.uuidString = uuidString
+    }
+
+    public init?(uuidString: String) {
+        self.timestamp48bit = Int64(Date().timeIntervalSince1970 * 1000) & 0x0000_FFFF_FFFF
+
+        self.uuidString = uuidString
     }
 
     private static func generateRandomBits(bitCount: Int) -> Data? {
@@ -60,6 +67,10 @@ public struct UUID7 {
         return Data(randomBytes)
     }
 
+    private static func generateRandomBitsFallback(bitCount: Int) -> Data {
+        
+    }
+    
     private static func setVersionAndVariantBits(randomBits: UInt64) -> UInt64 {
         let versionBits: UInt64 = 7 << 56  // Version 7
         let variantBits: UInt64 = 2 << 62  // Variant 1
