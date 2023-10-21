bdde9420c5958a3217cf980d2e8f2262b5566e82
Shota Shimazu <shotastage@users.noreply.github.com>
2023-10-22 08:48:30 +0900

    Update FastRandom.swift

--- Sources/DataLogic/Algorithms/FastRandom.swift
+++ Sources/DataLogic/Algorithms/FastRandom.swift
@@ -11,8 +11,25 @@ public struct NewFastRand {
     
 }
 
-struct Xorshift64 {
-    private var state: UInt64
+struct Xorshift128 {
+    private var state: (UInt32, UInt32, UInt32, UInt32)
+    
+    init(seed: (UInt32, UInt32, UInt32, UInt32)) {
+        self.state = seed
+    }
+    
+    mutating func random() -> UInt32 {
+        var s: (UInt32, UInt32, UInt32, UInt32) = self.state
+        var t: UInt32 = s.3
+        t ^= (t << 11)
+        t ^= (t >> 8)
+        s.3 = s.2
+        s.2 = s.1
+        s.1 = s.0
+        s.0 = (s.0 ^ (s.0 >> 19)) ^ (t ^ (t >> 14))
+        self.state = s
+        return s.0
+    }
 }
 
 
