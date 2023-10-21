f618f51be492672fc02ad6c84c1c2451e9292625
shotastage <shota.shimazu@magicalsoft.app>
2023-10-21 23:18:38 +0900

    implment random generator

---
+++ Sources/SwiftID/Algorithms/FastRandom.swift
@@ -0,0 +1,26 @@
+//
+//  FastPseudorandom.swift
+//
+//
+//  Created by Shota Shimazu on 2023/10/21.
+//
+
+import Foundation
+
+
+struct Xorshift {
+    private var state: UInt32
+
+    init(seed: UInt32) {
+        self.state = seed
+    }
+
+    mutating func random() -> UInt32 {
+        var x = self.state
+        x ^= (x << 13)
+        x ^= (x >> 17)
+        x ^= (x << 5)
+        self.state = x
+        return x
+    }
+}
