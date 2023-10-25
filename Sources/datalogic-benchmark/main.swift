0da2e1385a9ef19d9c68175a153965c9a4de5228
shotastage <shota.shimazu@magicalsoft.app>
2023-10-25 19:16:54 +0900

    add benchmark test

--- Sources/datalogic-benchmark/main.swift
+++ Sources/datalogic-benchmark/main.swift
@@ -1,10 +1,29 @@
 //
-//  File.swift
-//  
+//  main.swift
+//
 //
 //  Created by Shota Shimazu on 2023/10/25.
 //
 
-import Foundation
+import Benchmark
 
-print("Hello")
+let N = 10000
+var stringWithNoCapacity: String = ""
+var stringWithReservedCapacity: String = ""
+
+
+
+benchmark("Random Text Generation") {
+    let randomText = (0..<N).map { _ in
+        String((0..<10).map { _ in
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".randomElement()!
+        })
+    }.joined()
+    precondition(randomText.count == N * 10)
+}
+
+Benchmark.main()
+
+guard stringWithNoCapacity == stringWithReservedCapacity else {
+  fatalError("Unexpected Result.")
+}
